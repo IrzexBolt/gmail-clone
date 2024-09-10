40f1a00416bd64c0b5ee4ec4fe80569701ecaef0
IrzexGeek <irteza.nayani200@gmail.com>
2024-09-10 20:39:26 +0500

    key commit

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCaretDown, FaUserFriends } from "react-icons/fa";
 import { GoTag } from "react-icons/go";
 import { IoMdMore, IoMdRefresh } from "react-icons/io";
@@ -20,6 +20,7 @@ const mailType = [
 ];
 
 const Inbox = () => {
+  const [mailTypeSelected, setMailTypeSelected] = useState(0);
   return (
     <div className="flex-1 bg-white rounded-xl mx-5">
       <div className="flex items-center justify-between px-4">
@@ -40,7 +41,15 @@ const Inbox = () => {
         <div className="flex items-center gap-1">
           {mailType.map((item, index) => {
             return (
-              <button key={index}>
+              <button
+                key={index}
+                className={`${
+                  mailTypeSelected == index
+                    ? "border-b-4 border-b-blue-600 text-blue-600"
+                    : "border-b-4 border-b-transparent"
+                } flex items-center p-4 w-52 hover:bg-100`}
+                onClick={() => setMailTypeSelected(index)}
+              >
                 {item?.icon}
                 <span>{item?.text}</span>
               </button>
