9c649df0f20e802b37e717c8073f94887b79e03c
IrzexGeek <irteza.nayani200@gmail.com>
2024-09-10 19:54:47 +0500

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/shared/Navbar'
 import Sidebar from './components/Sidebar'
+import Body from './components/Body'
+import Inbox from './components/Inbox'
+import Mail from './components/Mail'
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<Body/>,
+  children:[
+    {
+    path:"/",
+    element:<Inbox/>
+    },
+    {
+      path:"/mail/:id",
+      element:<Mail/>
+    }
+  ]
+}])
 
 function App() {
    
   return (
     <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
       <Navbar/>
-      <Sidebar/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
