8d1a55d86f64052dbb5f6dc9b9806d7d99d4df9a
IrzexBolt <52386456+IrzexBolt@users.noreply.github.com>
2024-09-11 17:29:16 +0500

    env add

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBB7mBCp7IAsjMHi45Wv4RVIWlaMbJsbF4",
-  authDomain: "clone-yt-c6b9e.firebaseapp.com",
-  projectId: "clone-yt-c6b9e",
-  storageBucket: "clone-yt-c6b9e.appspot.com",
-  messagingSenderId: "1069921680327",
-  appId: "1:1069921680327:web:f9107e288d06614c4b77e2",
-  measurementId: "G-D96XWEVNJF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
